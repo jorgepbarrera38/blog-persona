c236b00aa7099a42d7e8bf1ff12c438630a3397d
Jorge Alexis Paz Barrera <jorgepbarrera38@gmail.com>
2019-05-24 09:33:24 -0500

    Agregado redireccion automática a https

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,13 +13,13 @@ class BlogController extends Controller
     }
 
     public function index () {
-        $user = User::select('name', 'info', 'photo', 'youtube', 'linkedin', 'github')->first();
+        $user = User::select('name', 'info', 'photo', 'youtube', 'linkedin', 'github')->firstOrFail();
         $posts = Post::published()->latest()->paginate(10);
         return view('blog.index', compact('posts', 'user'));
     }
 
     public function show (Post $blog) {
-        $user = User::select('name', 'info', 'photo', 'youtube', 'linkedin', 'github')->first();
+        $user = User::select('name', 'info', 'photo', 'youtube', 'linkedin', 'github')->firstOrFail();
         return view('blog.show', ['post' => $blog, 'user' => $user ]);
     }
 }
